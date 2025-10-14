1141f12efb6daf7f91d2aa9258542492db2925e9
Ashu3041 <ashutoshmohanty3041@gmail.com>
2025-10-14 13:40:56 +0530

    changes in asiosinstance

--- client/utils/axiosInstance.js
+++ client/utils/axiosInstance.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+// import dotenv from "dotenv";
+
+// dotenv.config();
 
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+console.log(BASE_URL);
 
 const axiosInstance = axios.create({
     baseURL:BASE_URL,
